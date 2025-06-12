060cd99c5dd38df706a9abdacf5b8835f15ba67d
Erick Salas <ericksc@gmail.com>
2025-06-12 17:52:39 +0000

    add gcp bucket storage def

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  project = "basic-python-app-462701"
+  region  = "us-central1"
+  # Optionally, specify credentials or let Terraform use GOOGLE_APPLICATION_CREDENTIALS
+  # credentials = file("<PATH_TO_SERVICE_ACCOUNT_JSON>")
+}
+
+
+resource "google_storage_bucket" "my-bucket" {
+  name          = "my-basic-app-python-bucket"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
